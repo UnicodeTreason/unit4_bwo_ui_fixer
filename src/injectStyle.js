976ec0ecd806396d748de79070764aec460a0419
tgxn <domenic@tgxn.net>
2020-10-27 17:57:20 +0800

    functionalize and correct loading issue

--- src/injectStyle.js
+++ src/injectStyle.js
@@ -1,19 +1,49 @@
 
-// check untill we are on the correct page to inject css
-if (document.URL.indexOf("ContentContainer.aspx") !== -1) {
-    console.log("on the ContentContainer, injecting Unit4 css");
+const contentContainerCSS = `
+.MainTable {
+    width: 100%;
+}
+#b_s89_g89s90 {
+    table-layout: auto;
+}
+`;
 
-    const contentContainerCSS = `
-        .MainTable {
-            width: 100% !important;
-        }
-    `;
+document.addEventListener('DOMContentLoaded', loadEvent);
 
-    const style = document.createElement('style');
-    style.type = 'text/css';
+function loadEvent(event) {
+    const documentUrl = document.URL;
+    const eventType = event.type;
+
+    const eventFileName = getUrlFileName(documentUrl);
+
+    console.info("😎 loadEvent", eventType, eventFileName);
+
+    // check we are on the correct page to inject css
+    if (eventFileName.startsWith("ContentContainer.aspx")) {
+        console.warn("on the ContentContainer!");
+        injectStyle(contentContainerCSS);
+    }
+
+    if (eventFileName.startsWith("CopyContainer.aspx")) {
+        console.warn("on the CopyContainer!");
+        injectStyle(contentContainerCSS);
+    }
+
+}
+
+function injectStyle(styleData) {
+    console.log("injectStyle");
 
     // add the css to the new style tag, as a text node
-    style.appendChild(document.createTextNode(contentContainerCSS));
+    const style = document.createElement('style');
+    style.id = 'unit4_style';
+    style.type = 'text/css';
+    style.rel = "stylesheet";
+    style.appendChild(document.createTextNode(styleData));
 
     document.head.appendChild(style);
+}
+
+function getUrlFileName(url) {
+    return url.replace(/\?.*$/, '').split('/').pop();
 }
